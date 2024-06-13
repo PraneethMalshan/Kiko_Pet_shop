b4a1563d931df1352265346348e3bf751543d544
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-06-13 06:57:07 +0530

    Git init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        Kiko_Pet_Shop
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All right reserved 
+      </footer>
+    </div>
+  )
+}
+
+export default App
