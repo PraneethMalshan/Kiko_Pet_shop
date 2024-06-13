d028d899bf1676abe83d2a3b29cb1ae480b87901
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-06-13 13:13:08 +0530

    Set page title

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export default function ProductPage() {
   return (
     <div>
-      ProductPage
+      <Helmet>
+        <title>Product Page</title>
+        
+      </Helmet>
+      Product Page
     </div>
   )
 }
