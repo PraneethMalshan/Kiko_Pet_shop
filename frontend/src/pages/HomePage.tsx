b728ef4e2391b78ee64c5758fdadc1cfaf4eed34
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-06-13 09:35:31 +0530

    Home page,s products Link to product page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,18 +1,21 @@
 import { Col, Row } from 'react-bootstrap'
 import { sampleProducts } from '../data'
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
   return (
     <Row>
         {sampleProducts.map((product) => (
             <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img 
-                    className='max-w-xs w-full' 
-                    src={product.image} 
-                    alt={product.name} 
-                />
-                <h2 className='text-3xl font-bold'>{product.name}</h2>
-                <p>${product.price}</p>
+                <Link to={'/product/' + product.slug}>
+                    <img 
+                        className='max-w-xs w-full' 
+                        src={product.image} 
+                        alt={product.name} 
+                    />
+                    <h2 className='text-3xl font-bold'>{product.name}</h2>
+                    <p>${product.price}</p>
+                </Link>
             </Col>
         ))}
     </Row>    
